3cac3625d63c5c933c59fb8f53379f3e4d5d3a9a
Vlad Povarna <povarna@adobe.com>
2022-09-20 09:51:50 +0300

    @trivial Adding Scala Doc on the Note case class

--- src/main/scala/io/notion/domain/Note.scala
+++ src/main/scala/io/notion/domain/Note.scala
@@ -1,5 +1,11 @@
 package io.notion.domain
 
+/**
+  * @param id = Unique random generated id. Used to identify a note
+  * @param title = Note title
+  * @param body = Note text
+  * @param createdAt = Date in milliseconds wen the note has been created
+  */
 final case class Note(
     id: Int,
     title: String,
